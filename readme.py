c81f99960d8580b6d5f50a07c8f7be3ca25f73ee
unknown <domfehgodfred12@gmail.com>
2024-09-10 03:57:16 -0700

    change readme.py

---
+++ readme.py
@@ -0,0 +1,9 @@
+git init
+
+touch - use to create project files eg (touch tim.py ,readme.txt)
+
+add . -use to add all file to the  repository
+
+git status -use to see the status of our  repository
+
+git commit -m "first change" -use to store versions whith a message
